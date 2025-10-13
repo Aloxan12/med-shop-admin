2d23c4398cc4e8d74d22da342853230b10620d68
Aloxan12 <kvakvaaaak@gmail.com>
2025-10-13 15:55:34 +0300

    route

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -2,7 +2,7 @@ import cls from "./Sidebar.module.scss";
 import { useSidebarItems } from "../model/hooks/useSidebarItems.ts";
 import { classNames, type Mods } from "@/shared/lib/classNames";
 import { AppFlex } from "@/shared/ui/AppFlex";
-import { useMemo } from "react";
+import { type ReactNode, useMemo } from "react";
 import { SidebarItem } from "@/widgets/Sidebar/ui/SidebarItem.tsx";
 
 interface SidebarProps {
@@ -24,7 +24,7 @@ export const Sidebar = ({ isOpen }: SidebarProps) => {
   return (
     <div className={classNames(cls.sidebarWrap, mods)}>
       <AppFlex direction="column" align="start" gap="20">
-        {itemsRender}
+        {itemsRender as ReactNode}
       </AppFlex>
     </div>
   );
