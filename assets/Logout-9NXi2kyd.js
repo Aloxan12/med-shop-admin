a2b7fa9c93b699d0807183de575bd01cd78faf6d
visiteur85 <visiteur85@users.noreply.github.com>
2025-11-03 08:27:18 +0000

    deploy: 81c9a5f724b03d21f4c27d96001001dd11b66f3a

---
+++ assets/Logout-9NXi2kyd.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Bm4K368i.js";const r=()=>t.jsx("div",{children:"LogOut"});export{r as default};
--- assets/MainPage-Cw-yNyhu.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-C0Gw6xzt.js";const r=()=>a.jsx("div",{children:"MainPage"});export{r as default};
