9643fa05752aa27612a45009aa587da1ae19be14
pavel <krivalcevich@gmail.com>
2025-11-05 10:24:18 +0300

    created icon for new user

--- src/widgets/AppTitleBlock/AppTitleBlock.tsx
+++ src/widgets/AppTitleBlock/AppTitleBlock.tsx
@@ -1,15 +1,21 @@
 import { AppFlex } from "@/shared/ui/AppFlex";
 import { AppTitle } from "@/shared/ui/AppTitle";
+import { UserRoundPlus } from "lucide-react";
+import { useState } from "react";
 
 interface AppTitleBlockProps {
   title: string;
 }
 
 export const AppTitleBlock = ({ title }: AppTitleBlockProps) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <AppFlex fullWidth justify="between">
       <AppTitle>{title}</AppTitle>
-      <AppFlex>actions</AppFlex>
+      <AppFlex>
+        <UserRoundPlus onClick={() => setOpen(true)} />
+      </AppFlex>
     </AppFlex>
   );
 };
