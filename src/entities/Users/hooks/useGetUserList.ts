ac2b2bccadf418a6741759579309d5fe84a82f2b
Aloxan12 <kvakvaaaak@gmail.com>
2025-12-05 14:00:52 +0300

    useGetUserList

---
+++ src/entities/Users/hooks/useGetUserList.ts
@@ -0,0 +1,16 @@
+import { useParamsControl } from "@/shared/lib/hooks/useParamsControl.ts";
+import type { GetUserListRequest } from "../model/types.ts";
+import { useUserList } from "../api/useUser.ts";
+
+export const useGetUserList = () => {
+  const params = useParamsControl<GetUserListRequest>({
+    paramsList: ["search"],
+    withPagination: true,
+  });
+  const { data: userList, isLoading } = useUserList(
+    { ...params },
+    { skip: !params },
+  );
+
+  return { userList, isLoading };
+};
--- src/entities/Users/ui/UsersTable/UsersTable.tsx
+++ src/entities/Users/ui/UsersTable/UsersTable.tsx
@@ -1,22 +1,15 @@
 import { AppTable } from "@/shared/ui/AppTable";
 import { userHeaderData } from "../../model/data.ts";
-import { useUserList } from "../../api/useUser.ts";
-import { useParamsControl } from "@/shared/lib/hooks/useParamsControl.ts";
 import "react-loading-skeleton/dist/skeleton.css";
-import type { GetUserListRequest } from "../../model/types.ts";
+import { useGetUserList } from "../../hooks/useGetUserList.ts";
 
 export const UsersTable = () => {
-  const params = useParamsControl<GetUserListRequest>({
-    paramsList: ["search"],
-    withPagination: true,
-  });
-  const { data, isLoading } = useUserList({ ...params }, { skip: !params });
-
+  const { userList, isLoading } = useGetUserList();
   return (
     <AppTable
       headerData={userHeaderData}
       isLoading={isLoading}
-      data={data}
+      data={userList}
       // почему нельзя этот массив вынести в переменную
       tableDataSelectors={[{ name: "id" }, { name: "email" }, { name: "role" }]}
     />
