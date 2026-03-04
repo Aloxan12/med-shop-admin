295bdc518f2bb026353cf323c7a8784f7164cfec
Aloxan12 <Aloxan12@users.noreply.github.com>
2026-03-04 06:17:42 +0000

    deploy: 0c66f8fbbf5c9b9ee30f7e5ca21ded59371d3df4

---
+++ assets/Logout-Ckkpw_rb.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DCVQPWsy.js";const r=()=>t.jsx("div",{children:"LogOut"});export{r as default};
---
+++ assets/MainPage-BPFfZ7Er.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-DCVQPWsy.js";const r=()=>a.jsx("div",{children:"MainPage"});export{r as default};
